325f22740cb91eb36516b2cbae61bf9ed9e9fc6e
Bruno Nascimento <sunr00t@gmail.com>
2022-10-09 14:35:50 -0300

    add base-url process env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('./config/express.js')();
 
 const port = process.env.PORT || 4000;
+const base_url = process.env.BASE_URL;
 
 /* eslint-disable-next-line */
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Listening at http://${base_url}:${port}`));
